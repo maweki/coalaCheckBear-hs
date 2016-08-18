370ce878b50dbdf48ef8ded48164787e8f32ad9d
Mario Wenzel <maweki@gmail.com>
2016-08-18 17:36:01 +0200

    inline

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,9 @@ import Coala
 import Coala.Input.SingleFile
 import qualified Data.Text as T
 
-needle = T.pack "Coala"
-msg = "Did you mean 'coala'?"
-
 check :: SingleFile -> [Result]
 check input = do (no, line) <- zip [1..] $ file input
-                 (pre, post) <- T.breakOnAll needle line
-                 let ofs = T.length pre
-                 return $ Result msg [codeRefLine (filename input) (Line no) (Just $ Column ofs) (+ 6)] (Severity 1)
+                 (pre, post) <- T.breakOnAll (T.pack "Coala") line
+                 return $ Result "Did you mean 'coala'?" [codeRefLine (filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)] (Severity 1)
 
 main = coala "coalaCheckBear" singleFile check
