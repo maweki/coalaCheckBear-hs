9b1fb736213e448582677fa616c8d275b8e284f9
Mario Wenzel <maweki@gmail.com>
2016-12-29 16:53:41 +0100

    use newer helpter

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,6 @@ import qualified Data.Text as T
 check :: SingleFile -> [Result]
 check input = do (no, line) <- zip [1..] $ file input
                  (pre, post) <- T.breakOnAll (T.pack "Coala") line
-                 return $ Result "Did you mean 'coala'?" [codeRefLine (filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)] (Severity 1)
+                 return $ Result "Did you mean 'coala'?" [codeRefInLine (filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)] (Severity 1)
 
 main = coala "coalaCheckBear" singleFile check
