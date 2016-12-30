232decc21ee4cf568aca8c953ba89b7990b0f96d
Mario Wenzel <maweki@gmail.com>
2016-12-30 12:43:23 +0100

    Updated example for updated bindings

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Coala
-import Coala.Input.SingleFile
+import qualified Coala.Input.SingleFile as Input
 import qualified Data.Text as T
 
-check :: SingleFile -> [Result]
-check input = do (no, line) <- zip [1..] $ file input
+check :: Input.SingleFile -> [Result]
+check input = do (no, line) <- zip [1..] $ Input.file input
                  (pre, post) <- T.breakOnAll (T.pack "Coala") line
-                 return $ Result "Did you mean 'coala'?" [codeRefInLine (filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)] (Severity 1)
+                 return $ Result Normal "Did you mean 'coala'?" [codeRefInLine (Input.filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)]
 
-main = coala "coalaCheckBear" singleFile check
+main = coala "coalaCheckBear" Input.singleFile check
