ce7e0375e12fcf8ab410aa54057c2a0a2636711d
Mario Wenzel <maweki@gmail.com>
2016-12-30 13:23:47 +0100

    Update to reflect FileRef typeclass

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Coala
-import qualified Coala.Input.SingleFile as Input
+import Coala.Input.SingleFile
 import qualified Data.Text as T
 
-check :: Input.SingleFile -> [Result]
-check input = do (no, line) <- zip [1..] $ Input.file input
+check :: SingleFile -> [Result]
+check input = do (no, line) <- zip [1..] $ file input
                  (pre, post) <- T.breakOnAll (T.pack "Coala") line
-                 return $ Result Normal "Did you mean 'coala'?" [codeRefInLine (Input.filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)]
+                 return $ Result Normal "Did you mean 'coala'?" [codeRefInLine (filename input) (Line no) (Just $ Column $ T.length pre) (+ 6)]
 
-main = coala "coalaCheckBear" Input.singleFile check
+main = coala "coalaCheckBear" singleFile check
